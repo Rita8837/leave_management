d56d445bebfec73a46904e7be0ef4d91e743175b
Rita8837 <Sarangerita4@gmail.com>
2025-06-08 20:46:14 +0300

    Delete leave_management directory

--- leave_management/app/apps.py
+++
@@ -1,9 +0,0 @@
-# app/apps.py
-from django.apps import AppConfig
-
-class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
-
-    def ready(self):
-        import app.signals  # Import signals to register them
--- leave_management/app/backends.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.backends import ModelBackend
-
-class UserSiteAuthBackend(ModelBackend):
-    def get_user(self, user_id):
-        return super().get_user(user_id)
